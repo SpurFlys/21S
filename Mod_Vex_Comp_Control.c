b2b47bb1e60b5eb2b2ee151e4c4a20e62097e34b
SpurFlys <SpurFlys@users.noreply.github.com>
2017-02-01 20:46:02 -0800

    Update Mod_Vex_Comp_Control.c

--- Mod_Vex_Comp_Control.c
+++ Mod_Vex_Comp_Control.c
@@ -1,10 +1,10 @@
-void allMotorsOff();
-void allTasksStop();
-void initIO();
-void pre_auton();
-task autonomous();
-task usercontrol();
-string sTeam;
+void allMotorsOff( );
+void allTasksStop( );
+void init( );
+void pre_auton( );
+task autonomous( );
+task teleop( );
+string strTeam;
 bool bLCDUserControl, bLCDAuton;
 
 #define clipNum(iInput, iHigh, iLow) iInput <= iLow ? iLow : (iInput >= iHigh ? iHigh : iInput)
@@ -12,110 +12,116 @@ bool bLCDUserControl, bLCDAuton;
 bool bStopTasksBetweenModes = true;
 
 void
-clearLCDLines () {
-	clearLCDLine(0);
-	clearLCDLine(1);
+waitForRelease( ) {
+	while( nLCDButtons != 0 ) { }
+	delay( 25 );
 }
 
 void
-clearAllIME () {
-	SensorValue[I2C_1] =
-	SensorValue[I2C_2] =
-	SensorValue[I2C_3] =
-	SensorValue[I2C_4] =
-	SensorValue[I2C_5] =
-	SensorValue[I2C_6] =
-	SensorValue[I2C_7] =
-	SensorValue[I2C_8] = 0;
+clearLCDLines( ) {
+	clearLCDLine( 0 );
+	clearLCDLine( 1 );
 }
 
 void
-waitForReleased7D () {
-	while (vexRT[Btn7D]){} wait1Msec(25);
+clearAllIME( ) {
+	SensorValue[ I2C_1 ] =
+	SensorValue[ I2C_2 ] =
+	SensorValue[ I2C_3 ] =
+	SensorValue[ I2C_4 ] =
+	SensorValue[ I2C_5 ] =
+	SensorValue[ I2C_6 ] =
+	SensorValue[ I2C_7 ] =
+	SensorValue[ I2C_8 ] = 0;
+}
+
+void
+waitForReleased7D( ) {
+	while( vexRT [ Btn7D ] ){ } wait1Msec( 25 );
 }
 
 task
-main() {
+main( ) {
 	bLCDBacklight = true;
-	clearDebugStream();
-	clearLCDLines();
-	initIO();
-	allMotorsOff();
-	displayLCDCenteredString(0, sTeam);
-	displayLCDCenteredString(1,  "Starting . . .");
-	wait1Msec(2000);
-	pre_auton();
+	clearDebugStream( );
+	clearLCDLines( );
+	init( );
+	allMotorsOff( );
+	displayLCDCenteredString( 0, strTeam );
+	displayLCDCenteredString( 1,  "Starting . . ." );
+	wait1Msec( 2000 );
+	pre_auton( );
 
-	while (true){
-		clearLCDLine(1);
-		if (bIfiAutonomousMode){
-			if (bLCDAuton) {
-				clearLCDLines();
-				displayLCDCenteredString(0, sTeam);
-				displayLCDCenteredString(1, "Autonomous");
+	while( true ) {
+		clearLCDLine( 1 );
+		if( bIfiAutonomousMode ) {
+			if( bLCDAuton ) {
+				clearLCDLines( );
+				displayLCDCenteredString( 0, strTeam );
+				displayLCDCenteredString( 1, "Autonomous" );
 			}
-			else clearLCDLines();
-			startTask(autonomous);
+			else clearLCDLines( );
+			startTask( autonomous );
 
-			while (bIfiAutonomousMode && !bIfiRobotDisabled){
-				if (!bVEXNETActive){
-					if (nVexRCReceiveState == vrNoXmiters)
-						allMotorsOff();
+			while( bIfiAutonomousMode && !bIfiRobotDisabled ) {
+				if( !bVEXNETActive ) {
+					if( nVexRCReceiveState == vrNoXmiters )
+						allMotorsOff( );
 				}
-				wait1Msec(25);
+				wait1Msec( 25 );
 			}
-			allMotorsOff();
-			if(bStopTasksBetweenModes)
-				allTasksStop();
+			allMotorsOff( );
+			if( bStopTasksBetweenModes )
+				allTasksStop( );
 		}
 
-		else{
-			if (bLCDUserControl) {
-				clearLCDLines();
-				displayLCDCenteredString(0, sTeam);
-				displayLCDCenteredString(1, "User Control");
+		else {
+			if( bLCDUserControl ) {
+				clearLCDLines( );
+				displayLCDCenteredString( 0, strTeam );
+				displayLCDCenteredString( 1, "User Control" );
 			}
-			else clearLCDLines();
-			startTask(usercontrol);
+			else clearLCDLines( );
+			startTask( teleop );
 
-			while (!bIfiAutonomousMode && !bIfiRobotDisabled){
-				if (nVexRCReceiveState == vrNoXmiters)
-					allMotorsOff();
-				wait1Msec(25);
+			while( !bIfiAutonomousMode && !bIfiRobotDisabled ) {
+				if( nVexRCReceiveState == vrNoXmiters )
+					allMotorsOff( );
+				wait1Msec( 25 );
 			}
-			allMotorsOff();
-			if(bStopTasksBetweenModes)
-				allTasksStop();
+			allMotorsOff( );
+			if( bStopTasksBetweenModes )
+				allTasksStop( );
 		}
 	}
 }
 
 
 void
-allMotorsOff() {
-	for (int i = 0; i < 10; i++)
-		motor[i] = 0;
+allMotorsOff( ) {
+	for( int i = 0; i < 10; i++ )
+		motor[ i ] = 0;
 }
 
 void
-allTasksStop() {
-	stopTask(1);
-	stopTask(2);
-	stopTask(3);
-	stopTask(4);
-	stopTask(5);
-	stopTask(6);
-	stopTask(7);
-	stopTask(8);
-	stopTask(9);
-	stopTask(10);
-	stopTask(11);
-	stopTask(12);
-	stopTask(13);
-	stopTask(14);
-	stopTask(15);
-	stopTask(16);
-	stopTask(17);
-	stopTask(18);
-	stopTask(19);
+allTasksStop( ) {
+	stopTask( 1  );
+	stopTask( 2  );
+	stopTask( 3  );
+	stopTask( 4  );
+	stopTask( 5  );
+	stopTask( 6  );
+	stopTask( 7  );
+	stopTask( 8  );
+	stopTask( 9  );
+	stopTask( 10 );
+	stopTask( 11 );
+	stopTask( 12 );
+	stopTask( 13 );
+	stopTask( 14 );
+	stopTask( 15 );
+	stopTask( 16 );
+	stopTask( 17 );
+	stopTask( 18 );
+	stopTask( 19 );
 }
